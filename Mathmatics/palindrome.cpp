a8750396234734975b9c66d200d04d0e74f1d2c2
Rethick <rethickpavan11@gmail.com>
2022-01-17 22:38:11 +0530

    added palindrome

--- Mathmatics/palindrome.cpp
+++ Mathmatics/palindrome.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+bool palindrome(int x);
+
+int main(){
+    int x;
+    cout << "Enter the number : ";
+    cin >> x;
+    string res =  (palindrome(x)==0)? "Not Palindrome":"Palindrome";
+    cout << res;
+}
+
+bool palindrome(int x){
+    int temp = x;
+    int res=0;
+    int remainder;
+    while(temp>0){
+        remainder = temp%10;
+        res = (res*10)+remainder;
+        temp = temp/10;
+        }
+    return (res==x);
+}
